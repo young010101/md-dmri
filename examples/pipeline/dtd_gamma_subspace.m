059cb30a3b1303c6d1b6b6389c8cf90b7b010324
Cheng Yang <yc5@tju.edu.cn>
2026-01-10 21:51:27 +0800

    Add data connection and processing steps in dtd_gamma_subspace.m

--- examples/pipeline/dtd_gamma_subspace.m
+++ examples/pipeline/dtd_gamma_subspace.m
@@ -223,3 +223,49 @@ subspace_xps = mdm_xps_from_bval_bvec(bval_fn, bvec_fn, b_delta_recon_all);
 %%
 recon_30 = squeeze(cat(ndims(recon_all.recon_fmac3),recon_all.recon_fmac3, recon_all.recon_fmac3_b_delta_1));
 disp(size(recon_30))
+
+%%
+% Connect to data
+ps = {};
+ps.bp = '/data/users/cyang/repos/HIFIVIM/Phantom/phan_cyan/';
+ps.ip = fullfile(ps.bp, 'DATA', 'brain', 'NII'); % <- actual input data
+ps.op = fullfile(ps.bp, 'processed', 'brain'); % <- store output here
+ps.zp = fullfile(ps.bp, 'tmp'); % <- store temporary files here
+
+if ((~exist(ps.bp, 'dir')) || ...
+        (~exist(ps.ip, 'dir')))
+    error('Data not found at specified folder. See instructions in header.')
+end
+
+
+msf_mkdir(ps.op); 
+msf_mkdir(ps.zp); 
+
+% Set options
+opt = mdm_opt;
+opt.do_overwrite = 1;
+opt.verbose      = 1;
+
+b_delta_lte = 1;
+b_delta_ste = 0;
+
+% Load linear and spherical data
+f = @(nii_fn, b_delta) mdm_s_from_nii(fullfile(ps.ip, nii_fn), b_delta);
+
+s = {...
+    f('recon_fmac3.nii.gz', b_delta_ste), ...
+    f('recon_fmac3_b_delta_1.nii.gz', b_delta_lte)};
+
+% Merge into one file
+s = mdm_s_merge(s, ps.op, 'FWF', opt);
+
+% Also make a powder-averaged data set for visualization
+% s.xps = rmfield(s.xps, 's_ind');
+% s_pa = mdm_s_powder_average(s, ps.op, opt);
+
+%%
+% Smooth the data a bit
+opt.filter_sigma = 0.6;
+
+% Do covariance analysis using all of the data
+dtd_gamma_pipe(s, ps.op, opt);
